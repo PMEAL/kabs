175e6768b48e294e196fae7184833e1d41fd6e7a
jgostick <jgostick@gmail.com>
2026-03-18 10:52:37 -0400

    making plots public

--- kabs/__init__.py
+++ kabs/__init__.py
@@ -2,7 +2,7 @@ from ._single_phase_solver import *
 from ._solve_flow import *
 from ._compute_permeability import *
 from ._compute_hydraulic_conductance import *
-from ._plots import *
+from .plots import *
 
 # Result containers (also exported via their respective solve modules' __all__)
 from ._solve_flow import FlowResult

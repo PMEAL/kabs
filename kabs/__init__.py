b297ef4df06947054ba13446894143294ed38dc4
jgostick <jgostick@users.noreply.github.com>
2026-03-21 02:31:48 +0000

    deploy: 7ab94cae7cf3abad1cb55d58dedeb9e12cf4d37c

--- kabs/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._single_phase_solver import *
-from ._solve_flow import *
-from ._compute_permeability import *
-from ._compute_hydraulic_conductance import *
-from .plots import *
-
-# Result containers (also exported via their respective solve modules' __all__)
-from ._solve_flow import FlowResult
--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import taichi as ti
-
-
-@pytest.fixture(scope="session", autouse=True)
-def taichi_init():
-    """Initialise Taichi once for the entire test session (CPU backend)."""
-    ti.init(arch=ti.cpu)

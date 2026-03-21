7a43447ad04037311b34a9154e216806a13aa1ed
jgostick <jgostick@gmail.com>
2026-03-20 23:44:15 -0400

    adding bump my version logic to pyproject file

--- kabs/__init__.py
+++ kabs/__init__.py
@@ -1,3 +1,16 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+
+    __version__ = version("kabs")
+except PackageNotFoundError:
+    import tomllib
+    from pathlib import Path
+
+    _pyproject = Path(__file__).parent.parent / "pyproject.toml"
+    with open(_pyproject, "rb") as _f:
+        __version__ = tomllib.load(_f)["project"]["version"]
+    del tomllib, Path, _pyproject, _f
+
 from ._single_phase_solver import *
 from ._solve_flow import *
 from ._compute_permeability import *
